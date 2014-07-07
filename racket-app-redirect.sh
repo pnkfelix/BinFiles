d56ce5bc5bc2955e4d6726437165932782993738
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-07-07 15:29:42 +0200

    Some old additions that I may not need but do not want to throw away.

--- racket-app-redirect.sh
+++ racket-app-redirect.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 ME=$(basename $0)
-exec /Applications/Racket\ v5.3.2/bin/$ME $@
+# exec /Applications/Racket\ v5.3.6/bin/$ME $@
+DIR=/Applications/Racket\ v5.93/bin
+if [ -e "$DIR/$ME" ] ; then
+    exec "$DIR/$ME" "$@"
+fi
+DIR=/Applications/Racket\ v5.3.6/bin
+if [ -e "$DIR/$ME" ] ; then
+    exec "$DIR/$ME" "$@"
+fi
