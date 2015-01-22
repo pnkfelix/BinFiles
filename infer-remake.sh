da590b584b44f0b500a52c62923b9848ff3f97a1
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-01-22 16:19:14 +0100

    updates to my infer-remake script that dynamically guesses between cargo or make

--- infer-remake.sh
+++ infer-remake.sh
@@ -1,9 +1,20 @@
-if ( echo $(pwd) | grep rust > /dev/null ); then
-    CMD="time remake -j1"
-else
-    CMD="time remake -j8"
-fi
+#!/bin/bash
 
+DIR="."
 
-echo "$CMD"
-$CMD
+while true; do
+    if [ -e "$DIR/Cargo.toml" ]; then
+        CMD="cd $DIR && cargo build && cargo test"
+        MSG='(flags of interest include `--verbose` and `-- --nocapture`)'
+        break;
+    elif [ -e "$DIR/Makefile" ]; then
+        CMD="time remake -C $DIR -j1"
+        MSG='(flags of interest include `--trace`)'
+        break;
+    fi
+done
+
+echo "$CMD $@"
+echo "$MSG"
+echo
+eval "$CMD" "$@"
