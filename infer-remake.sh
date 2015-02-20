aec08982bc956e98d6d00bcd71b168fff61c896f
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-02-20 18:08:21 +0100

    generalize infer-remake to support cargo as well.

--- infer-remake.sh
+++ infer-remake.sh
@@ -1,9 +1,15 @@
-if ( echo $(pwd) | grep rust > /dev/null ); then
-    CMD="time remake -j1"
-else
-    CMD="time remake -j8"
+if [ -e Makefile ]
+then
+    if ( echo $(pwd) | grep rust > /dev/null );
+    then
+        CMD="time remake -j1"
+    else
+        CMD="time remake -j8"
+    fi
+elif [ -e Cargo.toml ]
+then
+    CMD="time cargo test -- --nocapture"
 fi
-
 
 echo "$CMD"
 $CMD
