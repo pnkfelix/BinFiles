f21b758691096ce774c0f574ecb08bd75a4030bb
Felix S. Klock II <pnkfelix@pnkfx.org>
2018-02-20 13:49:56 +0100

    checkpoint initial support for x.py in my infer-remake script.

--- infer-remake.sh
+++ infer-remake.sh
@@ -2,8 +2,22 @@
 
 DIR="."
 
+. $HOME/ConfigFiles/Bash/search_for_repo.sh
+
+# CFG_SRC_DIR=$(grep -s 'CFG_SRC_DIR ' config.mk | awk '{print $3}')
+! search_parents_for_dotgit
+echo last_git_path $last_git_path
+CFG_SRC_DIR="$last_git_path/../"
+X_PY="${CFG_SRC_DIR}x.py"
+
 while true; do
-    if [ -e "$DIR/Cargo.toml" ]; then
+    if [ -e "$DIR/config.toml" -a -e "$X_PY" ]; then
+        echo X_PY=$X_PY
+        # CMD="time python $X_PY test src/tools/tidy && time python $X_PY build --stage 1 --incremental --keep-stage 0 src/libstd && time python $X_PY test --stage 1 src/test/{mir-opt,compile-fail,run-pass}"
+        CMD="time python $X_PY test src/tools/tidy && time python $X_PY build --stage 1 --incremental --verbose src/libstd && time python $X_PY test --stage 1 src/test/{mir-opt,compile-fail,run-pass}"
+        MSG='(flags of interest include `--stage 1` and `--help`)'
+        break;
+    elif [ -e "$DIR/Cargo.toml" ]; then
         CMD="cd $DIR && cargo build && cargo test"
         MSG='(flags of interest include `--verbose` and `-- --nocapture`)'
         break;
@@ -12,6 +26,8 @@ while true; do
         MSG='(flags of interest include `--trace`)'
         break;
     fi
+    echo "build config unfound in $(pwd); moving up a directory"
+    cd ..
 done
 
 echo "$CMD $@"
