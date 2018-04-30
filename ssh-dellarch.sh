caa7fceeb89f5f8852d9b6779986f2d7a4a2c479
Felix S. Klock II <pnkfelix@pnkfx.org>
2018-04-30 15:30:39 +0200

    update the dellarch connection.

--- ssh-dellarch.sh
+++ ssh-dellarch.sh
@@ -1,1 +1,6 @@
-ssh -A -t people.mozilla.org ssh localhost -p 61017
+# DELLARCH=10.243.30.83
+# DELLARCH=10.243.29.112
+DELLARCH=10.243.28.27
+
+# ssh -A -t people.mozilla.org ssh localhost -p 61017
+ssh -A -t ssh.mozilla.com ssh pnkfelix@$DELLARCH
