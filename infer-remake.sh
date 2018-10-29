c453966a0c0dc147858f3148ea733a4df2d3b80a
Felix S. Klock II <pnkfelix@pnkfx.org>
2018-10-30 00:58:19 +0100

    update to latest best pracices infer-remake.sh

--- infer-remake.sh
+++ infer-remake.sh
@@ -11,10 +11,16 @@ CFG_SRC_DIR="$last_git_path/../"
 X_PY="${CFG_SRC_DIR}x.py"
 
 # X_PY_TESTS="src/test/{mir-opt,compile-fail,run-pass}"
-X_PY_TESTS="src/test/{compile-fail,ui,run-pass,mir-opt}"
+X_PY_TESTS="src/test/{ui,compile-fail,run-pass,mir-opt}"
+
+ONLY_BUILD=0
 
 arg=$1
-if [ "x$arg" = "xstage1" ] ; then
+if [ "x$arg" = "xbuild" ]; then
+    shift
+    echo "Shifting \`$arg\` off args list; left with [$@]"
+    ONLY_BUILD=1
+elif [ "x$arg" = "xstage1" ] ; then
     shift
     echo "Shifting \`$arg\` off args list; left with [$@]"
     X_PY_FLAGS="--stage 1 "
@@ -22,15 +28,30 @@ elif [ "x$arg" = "xstage2" ] ; then
     shift
     echo "Shifting \`$arg\` off args list; left with [$@]"
     X_PY_FLAGS="--stage 2 "
+    X_PY_TESTS="src/test/{compile-{fail,fail-fulldeps},ui,ui-fulldeps,run-{pass,fail,pass-fulldeps,fail-fulldeps},mir-opt,codegen,codegen-units,incremental,incremental-fulldeps,parse-fail}"
 else
-    X_PY_FLAGS="--stage 1 --incremental "
+    # X_PY_FLAGS="--stage 1 --incremental "
+    # X_PY_FLAGS="--keep-stage 1 --stage 1 "
+    X_PY_FLAGS="--keep-stage 1 --stage 1 "
+    # X_PY_FLAGS=" --stage 1 "
 fi
-    
+
+
 while true; do
     if [ -e "$DIR/config.toml" -a -e "$X_PY" ]; then
         echo X_PY=$X_PY
-        # CMD="time python $X_PY test src/tools/tidy && time python $X_PY build --stage 1 --incremental --keep-stage 0 src/libstd && time python $X_PY test --stage 1 src/test/{mir-opt,compile-fail,run-pass}"
-        CMD="time python $X_PY test $X_PY_FLAGS src/tools/tidy && time python $X_PY build $X_PY_FLAGS src/libstd && time python $X_PY test $X_PY_FLAGS $X_PY_TESTS"
+
+        TIDY=" time python $X_PY test  --stage 1 src/tools/tidy"
+        BUILD="time RUSTC_FLAGS=-Ztreat-err-as-bug python $X_PY build $X_PY_FLAGS src/libstd"
+        CHECK="time python $X_PY check $X_PY_FLAGS "
+        TESTS="time python $X_PY test  $X_PY_FLAGS $X_PY_TESTS"
+
+        if [ "x$ONLY_BUILD" = "x1" ]; then
+            CMD="$TIDY && $BUILD "
+        else
+            # CMD="$TIDY && $BUILD && $CHECK && $TESTS"
+            CMD="$TIDY && $BUILD && $TESTS"
+        fi
         MSG='(flags of interest include `--stage 1` and `--help`)'
         break;
     elif [ -e "$DIR/Cargo.toml" ]; then
