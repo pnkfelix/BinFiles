e365504d5410d4cfeb14ee4907ed6c45440ceea1
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-03-04 13:30:54 +0100

    Added support for multiple cmdline arguemtns: notidy/clean/build/stage1/stage2.
    (I had *thought* that I had made a sleep step part of cleaning, but I
    dont see that there now...)

--- infer-remake.sh
+++ infer-remake.sh
@@ -14,34 +14,55 @@ X_PY="${CFG_SRC_DIR}x.py"
 X_PY_TESTS="src/test/{ui,compile-fail,run-pass,mir-opt}"
 
 ONLY_BUILD=0
+NO_TIDY=0
+CLEAN_FIRST=0
 
-arg=$1
-if [ "x$arg" = "xbuild" ]; then
-    shift
-    echo "Shifting \`$arg\` off args list; left with [$@]"
-    ONLY_BUILD=1
-elif [ "x$arg" = "xstage1" ] ; then
-    shift
-    echo "Shifting \`$arg\` off args list; left with [$@]"
-    X_PY_FLAGS="--stage 1 "
-elif [ "x$arg" = "xstage2" ] ; then
-    shift
-    echo "Shifting \`$arg\` off args list; left with [$@]"
-    X_PY_FLAGS="--stage 2 "
-    X_PY_TESTS="src/test/{compile-{fail,fail-fulldeps},ui,ui-fulldeps,run-{pass,fail,pass-fulldeps,fail-fulldeps},mir-opt,codegen,codegen-units,incremental,incremental-fulldeps}"
-else
-    # X_PY_FLAGS="--stage 1 --incremental "
-    # X_PY_FLAGS="--keep-stage 1 --stage 1 "
-    X_PY_FLAGS="--keep-stage 1 --stage 1 "
-    # X_PY_FLAGS=" --stage 1 "
-fi
-
+for arg in "$@"
+do
+    if [ "x$arg" = "xnotidy" ] ; then
+        shift
+        echo "Shifting \`$arg\` off args list; left with [$@]"
+        NO_TIDY=1
+    elif [ "x$arg" = "xclean" ]; then
+        shift
+        echo "Shifting \`$arg\` off args list; left with [$@]"
+        echo "Sleeping for 5 seconds before initiating clean."
+        sleep 5
+        CLEAN_FIRST=1
+    elif [ "x$arg" = "xbuild" ]; then
+        shift
+        echo "Shifting \`$arg\` off args list; left with [$@]"
+        ONLY_BUILD=1
+    elif [ "x$arg" = "xstage1" ] ; then
+        shift
+        echo "Shifting \`$arg\` off args list; left with [$@]"
+        X_PY_FLAGS="--stage 1 "
+    elif [ "x$arg" = "xstage2" ] ; then
+        shift
+        echo "Shifting \`$arg\` off args list; left with [$@]"
+        X_PY_FLAGS="--stage 2 "
+        X_PY_TESTS="src/test/{compile-{fail,fail-fulldeps},ui,ui-fulldeps,run-{pass,fail,pass-fulldeps,fail-fulldeps},mir-opt,codegen,codegen-units,incremental,incremental-fulldeps}"
+    else
+        # X_PY_FLAGS="--stage 1 --incremental "
+        # X_PY_FLAGS="--keep-stage 1 --stage 1 "
+        X_PY_FLAGS="--keep-stage 1 --stage 1 "
+        # X_PY_FLAGS=" --stage 1 "
+    fi
+done
 
 while true; do
     if [ -e "$DIR/config.toml" -a -e "$X_PY" ]; then
         echo X_PY=$X_PY
 
+        if [ $CLEAN_FIRST == 1 ]; then
+            echo "Cleaning"
+            eval "$X_PY clean"
+        fi
+
         TIDY=" time python $X_PY test  --stage 1 src/tools/tidy"
+        if [ $NO_TIDY == 1 ]; then
+            TIDY="true"
+        fi
         BUILD="time RUSTC_FLAGS=-Ztreat-err-as-bug python $X_PY build $X_PY_FLAGS src/libstd"
         CHECK="time python $X_PY check $X_PY_FLAGS "
         TESTS="time python $X_PY test  $X_PY_FLAGS $X_PY_TESTS"
