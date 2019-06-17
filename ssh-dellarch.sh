7adbec761b2831e701b5856d2f75cb1d66d7b208
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-06-17 14:41:42 +0200

    Extend ssh script to connect directly to Dell machine when on local subnet.

--- ssh-dellarch.sh
+++ ssh-dellarch.sh
@@ -2,5 +2,17 @@
 # DELLARCH=10.243.29.112
 DELLARCH=10.243.28.27
 
-# ssh -A -t people.mozilla.org ssh localhost -p 61017
-ssh -A -t ssh.mozilla.com ssh pnkfelix@$DELLARCH
+ifconfig | grep "inet " | grep -v 127.0.0.1 | grep 10.243
+VALUE=$?
+
+if [ $VALUE == 0 ]; then
+    echo "We are on Mozilla local net; directly connecting"
+
+    ssh -A -t pnkfelix@$DELLARCH
+else
+    echo "We are outside Mozilla local net; connecting via ssh bridge"
+
+    # ssh -A -t people.mozilla.org ssh localhost -p 61017
+    ssh -A -t fklock@ssh.mozilla.com ssh pnkfelix@$DELLARCH
+
+fi
